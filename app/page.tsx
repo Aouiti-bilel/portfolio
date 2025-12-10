f3ac20da40b097051971b77ffce6accf1f0b2103
Aouiti-bilel <khelifa.bilel1@outlook.fr>
2025-12-10 13:14:53 +0100

    wip: adding env variabels

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 
 import Hero from "@/components/Hero";
-import HeadToHeadSection from "@/components/HeadToHeadSection";
 import DocumentationPreview from "@/components/DocumentationPreview";
-import RecentPosts from "@/components/RecentPosts";
-import Footer from "@/components/Footer";
 import ProjectsHero from "@/components/ProjectsHero";
 import ModulesHero from "@/components/ModulesHero";
 
