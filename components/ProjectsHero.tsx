55398e2a4ef239cc19466bb4189ade0c06132649
Aouiti-bilel <khelifa.bilel1@outlook.fr>
2025-12-10 14:00:48 +0100

    Fix

--- components/ProjectsHero.tsx
+++ components/ProjectsHero.tsx
@@ -20,14 +20,30 @@ export default function ProjectsPage() {
     arrows: true,
     responsive: [
       {
-        breakpoint: 1280, // xl
-        settings: { slidesToShow: 2 },
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
       },
       {
-        breakpoint: 768, // md
-        settings: { slidesToShow: 1 },
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
       },
-    ],
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
 
   return (
